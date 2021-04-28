1d7815c27166a73e7e8ec0295d6ac60ce4744802
Matt Moore <Matt.M.Moore13@outlook.com>
2021-04-28 03:10:38 -0400

    Refactored the App.js page to handle React Hooks.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
+import React from 'react'
+import { withRouter } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = props => (
+  <React.Fragment>
+    <h3>{props.location.state ? props.location.state.msg : null}</h3>
+  </React.Fragment>
+)
 
-export default App;
+export default withRouter(App)
+
